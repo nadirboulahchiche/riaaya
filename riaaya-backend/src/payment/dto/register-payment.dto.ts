27baf291c23b2a05c0266eca02ece565b78bb726
nadirboulahchiche <95067549+nadirboulahchiche@users.noreply.github.com>
2025-12-10 14:58:51 +0100

    Add validation to RegisterPaymentDto

--- riaaya-backend/src/payment/dto/register-payment.dto.ts
+++ riaaya-backend/src/payment/dto/register-payment.dto.ts
@@ -1,1 +1,15 @@
+import { IsNumber, IsString, IsOptional, Min, Max } from 'class-validator';
 
+export class RegisterPaymentDto {
+  @IsNumber()
+  @Min(1000)
+  @Max(5000)
+  amount: number;
+
+  @IsString()
+  appointmentId: string;
+
+  @IsString()
+  @IsOptional()
+  language?: string;
+}
